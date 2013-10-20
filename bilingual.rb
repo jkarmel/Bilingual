9ac54583ebd62439bab6f077f4a14d781095aa4d
Jeremy Karmel <jkarmel@me.com>
2013-10-19 21:54:57 -0700

    choose js class

--- bilingual.rb
+++ bilingual.rb
@@ -6,6 +6,7 @@ module Bilingual
   CONTEXT = ExecJS.compile File.open('test.js').read
   CONTEXT.eval "objects = []"
 
+
   def method_missing *args
     method_name = args[0].to_s.camelize(:lower)
     prop =  "objects[#{@js_index}].#{method_name}"
@@ -18,13 +19,26 @@ module Bilingual
   end
 
   def initialize
-    CONTEXT.eval("objects.push(new TestClass)")
+    CONTEXT.eval("objects.push(new #{self.class.js_class_name})")
     @js_index = CONTEXT.eval("objects.length") - 1
   end
+
+  def self.included(base)
+    base.extend(ClassMethods)
+  end
+
+  module ClassMethods
+    attr_accessor :js_class_name
+    def set_js_class class_name
+      self.js_class_name = class_name
+    end
+  end
+
 end
 
 class TestClass
   include Bilingual
+  set_js_class 'TestClass'
   def call_js_from_ruby
     from_js + " and from ruby land"
   end
